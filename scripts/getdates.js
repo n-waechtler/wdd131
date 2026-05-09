33ea37aa89f557b2a07733ef070e2b5f0fab4e5e
n-waechtler <njwaechtler89@gmail.com>
2026-05-09 10:20:45 -0600

    js update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,5 @@
-// Set the current year
-const yearSpan = document.getElementById("currentyear");
+const yearSpan = document.querySelector("#currentyear");
 yearSpan.textContent = new Date().getFullYear();
 
-// Set the last modified date
-const lastModP = document.getElementById("lastModified");
-lastModP.textContent = "Last Modification: " + document.lastModified;
+const lastMod = document.querySelector("#lastModified");
+lastMod.textContent = `Last Modification: ${document.lastModified}`;
