e0584dc2d535e45e7a839f168ada696024836c5e
n-waechtler <njwaechtler89@gmail.com>
2026-05-09 10:28:23 -0600

    updated js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,8 @@
-const yearSpan = document.querySelector("#currentyear");
+// 1. Get the current year and put it in the span
+const yearSpan = document.getElementById("currentyear");
 yearSpan.textContent = new Date().getFullYear();
 
-const lastMod = document.querySelector("#lastModified");
-lastMod.textContent = `Last Modification: ${document.lastModified}`;
+// 2. Get the last modified date of the document and put it in the second paragraph
+const lastModifiedPara = document.getElementById("lastModified");
+lastModifiedPara.textContent = `Last Modification: ${document.lastModified}`;
+
