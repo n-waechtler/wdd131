d19d06b05109a57f5e9407322a4d045f931dc19e
n-waechtler <njwaechtler89@gmail.com>
2026-05-15 20:17:22 -0600

    added to js file

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,12 @@ const yearSpan = document.getElementById("currentyear");
 yearSpan.textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
