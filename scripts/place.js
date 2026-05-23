d43163a7d5f1a053d9d5d837149fa1e61700476d
n-waechtler <njwaechtler89@gmail.com>
2026-05-22 21:39:25 -0600

    js update

--- scripts/place.js
+++ scripts/place.js
@@ -2,3 +2,23 @@ const yearSpan = document.getElementById("currentyear");
 yearSpan.textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const currentTemp = 10;
+const currentWind = 5;
+
+function calculateWindChill(temp, speed) {
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
+}
+
+let windChillOutput = "N/A";
+
+if (currentTemp <= 10 && currentWind > 4.8) {
+    windChillOutput = `${calculateWindChill(currentTemp, currentWind)} °C`;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const windChillField = document.getElementById("windchill-value");
+    if (windChillField) {
+        windChillField.textContent = windChillOutput;
+    }
+});
