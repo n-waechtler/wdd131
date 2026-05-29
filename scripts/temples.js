6427bc293ea3611099fc95e06273c072202126b4
n-waechtler <njwaechtler89@gmail.com>
2026-05-28 22:25:19 -0600

    hamburger button fix

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,9 +5,21 @@ document.getElementById("lastModified").innerHTML = document.lastModified;
 
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
+const title = document.querySelector('header p')
 
-hambutton.addEventListener('click', () => {
+hambutton.addEventListener('click', (e) => {
+    e.preventDefault();
+    
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
+
+    if (window.innerWidth < 1000) {
+        
+        if (hambutton.classList.contains('show')) {
+            title.classList.add('hide');
+        } else {
+            title.classList.remove('hide');
+        }
+    }
 });
 
