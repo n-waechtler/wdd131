f59c3b06cab0c93f1d5f37afc6a86a5de65b353d
n-waechtler <njwaechtler89@gmail.com>
2026-06-13 17:33:41 -0600

    project updates

---
+++ project/scripts/lego-history.js
@@ -0,0 +1,31 @@
+const yearSpan = document.getElementById("currentyear");
+yearSpan.textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+const title = document.querySelector('header h1');
+
+hambutton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+
+    if (window.innerWidth < 1000) {
+        if (hambutton.classList.contains('show')) {
+            title.classList.add('hide');
+        } else {
+            title.classList.remove('hide');
+        }
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 1000) {
+        title.classList.remove('hide');
+    } else if (hambutton.classList.contains('show')) {
+        title.classList.add('hide');
+    }
+});
